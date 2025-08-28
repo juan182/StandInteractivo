bd1925d8952e05ec48832bc25a13f41ff9ecd4de
juan182 <60664888+juan182@users.noreply.github.com>
2025-08-28 17:40:16 -0500

    Aplicacion del GC y adicion de carga de escena
    Agregue el game controller en la herarquia dentro del main, adicionalmente cree el codigo para cargar escenas y mantener un registro de codigos

---
+++ Parcial1RV/Assets/Script/Manager/LoadScene.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string Code ="";
+    //Nota: modificar al adaptar la interfaz
+    public string TypedCode = "";
+
+    public GameObject panelNull;
+    public GameObject panelError;
+    public GameObject panelCorrect;
+    /// <summary>
+    /// Carga la escena especificada por su nombre. Si existe un temporizador, 
+    /// lo inicia.
+    /// </summary>
+    /// <param name="nameScene">Nombre de la escena a cargar.</param>
+    public void Load(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+
+    }
+
+    /// <summary>
+    /// Controla los paneles que permiten acceso a la escena,
+    /// </summary>
+    /// <param panelNull>Advertencia que el usuario no escribio nada</param>
+    /// <param panelError>Advertencia que el usuario escribio el codigo incorrecto</param>
+    /// <param panelCorrect>Abre el panel que permite cambiar de escena</param>
+    public void SceneControll()
+    {
+        if (TypedCode == null)
+        {
+            panelNull.SetActive(true);
+        }
+        else if (TypedCode != Code)
+        {
+            panelError.SetActive(true);
+        }
+        else
+        {
+            panelCorrect.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// Cierra la aplicación.
+    /// </summary>
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
